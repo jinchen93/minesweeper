1e69a319da9761199ce27216b44876c9bd63db0c
App Academy Student <student@appacademy.io>
2017-03-30 13:47:36 -0700

    setup Tile class

--- tile.rb
+++ tile.rb
@@ -1,3 +1,37 @@
 class Tile
-  
+  attr_reader :flagged, :bomb
+
+  def initialize(bomb)
+    @bomb = bomb
+    @flagged = false
+    @visible = false
+
+    @neighbors = []
+  end
+
+  def toggle_flag
+    @flagged = !flagged
+  end
+
+  def reveal
+    @visible = true
+  end
+
+  def bombed?
+    bomb
+  end
+
+  def visible?
+    @visible
+  end
+
+  def flagged?
+    @flagged
+  end
+
+  def neighbor_bomb_count
+    @neighbors.count(&:bombed?)
+  end
+
+  def add_neighbor(neighbor) end
 end
